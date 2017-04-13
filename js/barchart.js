cdf7bb46e6c3ccc3942aaab12ed78240c160df07
Katrina Hirsch <katrina.hirsch@gmail.com>
2017-04-13 00:30:37 -0700

    create svg canvas

--- js/barchart.js
+++ js/barchart.js
@@ -6,3 +6,17 @@ var snowpack = [
 var periods = snowpack.map(function(t) {
   return t.period
 });
+
+var margin = {top: 5, right: 5, bottom: 50, left: 50};
+
+var fullWidth = 700;
+var fullHeight = 200;
+
+var width = fullWidth - margin.right - margin.left;
+var height = fullHeight - margin.top - margin.bottom;
+
+var svg = d3.select('body').append('svg')
+  .attr('width', fullWidth)
+  .attr('height', fullHeight)
+  .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
