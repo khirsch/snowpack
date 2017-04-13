1a678ab933ea2c46464c75ffab678e5c3212d7a5
Katrina Hirsch <katrina.hirsch@gmail.com>
2017-04-13 05:27:17 -0700

    include more specific date ranges

--- js/barchart.js
+++ js/barchart.js
@@ -1,6 +1,6 @@
 var snowpack = [
-  {inches: 340.1, period: '2014 - 2015'},
-  {inches: 306.6, period: '2016 - Present'}
+  {inches: 340.1, period: 'Oct 2014 - May 2015'},
+  {inches: 306.6, period: 'Oct 2016 - Present'}
 ];
 
 var periods = snowpack.map(function(t) {
